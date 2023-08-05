bfafa118a7a3b830dd3a84348a80d169f2aa6807
Rich <applerich0306@gmail.com>
2023-08-05 21:56:42 +0300

    modify Usee class

--- backend/Domain/src/Entities/User.cs
+++ backend/Domain/src/Entities/User.cs
@@ -7,6 +7,15 @@ namespace Domain.src.Entities
 {
     public class User : BaseEntityWithId
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+    public enum Role
+    {
+        Admin,
+        User
     }
 }
