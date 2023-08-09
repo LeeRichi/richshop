ae0585d666197c9eab8e207f19ba5a5a294f865b
Rich <applerich0306@gmail.com>
2023-08-09 16:17:26 +0300

    basic setup

---
+++ src/Abstraction/IProductService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.src.Entities;
+using Business.src.Dtos;
+
+namespace Business.src.Abstraction
+{
+    public interface IProductService : IBaseService<Product, ProductDto>
+    {
+        
+    }
+}
