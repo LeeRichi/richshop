b9e60b1f201bd7b2af8d37262382573785621c8f
Rich <applerich0306@gmail.com>
2023-08-10 10:57:31 +0300

    fix err in Business layer

--- backend/Controller/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Controller;
-
-public class Class1
-{
-
-}
---
+++ backend/Controller/src/Controllers/CrudController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controller.src.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]s")]
+    public class CrudController : ControllerBase
+    {
+        
+    }
+}
