9e76e76839411713033d4eafa27c810f1b6305f0
Rich <applerich0306@gmail.com>
2023-08-10 12:00:22 +0300

    add CrudController

--- backend/Controller/src/Controllers/CrudController.cs
+++ backend/Controller/src/Controllers/CrudController.cs
@@ -8,8 +8,42 @@ namespace Controller.src.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]s")]
-    public class CrudController : ControllerBase
+    public class CrudController<T, TReadDto, TCreateDto, TupdateDto> : ControllerBase
     {
-        
+        private readonly IBaseService<T, TReadDto, TCreateDto, TupdateDto> _baseService;
+
+        public CrudController(IBaseService<T, TReadDto, TCreateDto, TupdateDto> BaseService){
+            _baseService = BaseService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TReadDto>>> GetAll([FromQuery]QueryOptions queryOptions){
+            return Ok(await _baseService.GetAll(queryOptions));
+        }
+
+        [HttpGet("{id}")]
+        public async TaskTask<ActionResult<TReadDto>> GetOneById ([FromRoute]string id){
+            return Ok(await _baseService.GetOneById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TReadDto>> CreateOne([FromBody] TupdateDto dto){
+            var createObj = _baseService.CreateOne(dto);
+            return CreateAtAction("Created", createObj);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<TReadDto>> UpdateOneById([FromRoute] string id,[FromForm] TupdateDto update){
+            var updateObj = await _baseService.UpdateOneById(id, update);
+            return Ok(UpdateOneById);
+        }
+
+        [HttpDelete("{id}")]
+        public async Tasl<ActionResult<bool>> DeleteOneById([FromRoute] string id){
+            return StatusCode(204, await _baseService.DeleteOneById(id));
+        }
+
     }
+
+    
 }
