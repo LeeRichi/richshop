0dcbba09e973ecdb91784b9d34d6b4ee9c7702f5
Rich <applerich0306@gmail.com>
2023-08-10 23:59:42 +0300

    add ProductController

---
+++ backend/Controller/src/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controller.src.Controllers
+{
+    public class ProductController
+    {
+        
+    }
+}
