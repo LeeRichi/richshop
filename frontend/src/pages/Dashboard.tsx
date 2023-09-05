1b8ad06bdc039ed25b1b3f65cb7b27f1cab64084
Rich <applerich0306@gmail.com>
2023-09-05 23:40:10 +0300

    modify dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -73,6 +73,7 @@ export const Dashboard: React.FC<DashboardProps> = ({ name, avatar, role, userId
   const fetchUserDetails = async (userId: any) =>
   {
     try {
+      console.log(`userId: ${userId}`)
       const response = await axios.get(`https://fullstackshop.azurewebsites.net/api/v1/users/${userId}`);
       setUserDetails(response.data);
       console.log(userDetails)
@@ -151,152 +152,155 @@ export const Dashboard: React.FC<DashboardProps> = ({ name, avatar, role, userId
 
   return (
     <Container maxWidth="md" sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "5rem" }}>
-      {role === 'Admin' ? (
-        <>
-          <Typography variant="h4" align="center" gutterBottom sx={{ marginBottom: "3rem" }}>
-            <IoMdHand /> Hi, <img src={avatar} alt={`avatar`} width="50" style={{ verticalAlign: 'middle' }} />
-            {name}, what are you thinking?
+      {userDetails != null ? ( <>
+        {role === 'Admin' ? (
+          <>
+            <Typography variant="h4" align="center" gutterBottom sx={{ marginBottom: "3rem" }}>
+              <IoMdHand /> Hi, <img src={avatar} alt={`avatar`} width="50" style={{ verticalAlign: 'middle' }} />
+              {name}, what are you thinking?
+            </Typography>
+            <IconButton
+              color="primary"
+              sx={{ borderRadius: 8, marginBottom: "1rem" }}
+              component={Link} to="/products"
+            >
+              <EditIcon fontSize="large" />
+              <Typography variant="h5" align="center" gutterBottom>
+                Manage Products
+              </Typography>
+            </IconButton><br />
+            <IconButton
+              color="primary"
+              sx={{ borderRadius: 8, marginBottom: "1rem" }}
+              component={Link} to="/users"
+            >
+              <PersonIcon fontSize="large" />
+              <Typography variant="h5" align="center" gutterBottom>
+                Manage Users
+              </Typography>
+            </IconButton><br />
+            <IconButton
+              color="primary"
+              sx={{ borderRadius: 8, marginBottom: "1rem" }}
+              component={Link} to="/orders"
+            >
+              <ShoppingBasketIcon fontSize="large" />
+              <Typography variant="h5" align="center" gutterBottom>
+                Manage Orders
+              </Typography>
+            </IconButton>
+          </>
+        ) : (
+          <Typography variant="h4" align="center" gutterBottom sx={{marginBottom: "3rem"}}>
+            Hi, <img src={avatar} alt={`avatar`} width="50" style={{ verticalAlign: 'middle' }} />{userDetails.name}, welcome back!
           </Typography>
-          <IconButton
-            color="primary"
-            sx={{ borderRadius: 8, marginBottom: "1rem" }}
-            component={Link} to="/products"
-          >
-            <EditIcon fontSize="large" />
-            <Typography variant="h5" align="center" gutterBottom>
-              Manage Products
-            </Typography>
-          </IconButton><br />
-          <IconButton
-            color="primary"
-            sx={{ borderRadius: 8, marginBottom: "1rem" }}
-            component={Link} to="/users"
-          >
-            <PersonIcon fontSize="large" />
-            <Typography variant="h5" align="center" gutterBottom>
-              Manage Users
-            </Typography>
-          </IconButton><br />
-          <IconButton
-            color="primary"
-            sx={{ borderRadius: 8, marginBottom: "1rem" }}
-            component={Link} to="/orders"
-          >
-            <ShoppingBasketIcon fontSize="large" />
-            <Typography variant="h5" align="center" gutterBottom>
-              Manage Orders
-            </Typography>
-          </IconButton>
-        </>
-      ) : (
-        <Typography variant="h4" align="center" gutterBottom sx={{marginBottom: "3rem"}}>
-          Hi, <img src={avatar} alt={`avatar`} width="50" style={{ verticalAlign: 'middle' }} />{name}, welcome back!
-        </Typography>
-      )}
+        )}
 
-      {role === 'User' && userDetails && (
-        <>
-          <Typography variant="h6" align="center" gutterBottom>
-            Your UserID: {userId}
-          </Typography>
-          <Typography variant="body1" align="center" gutterBottom>
-            Name: {name}
-          </Typography>
-          <img src={avatar} alt={`avatar`} width="100" style={{ cursor: 'pointer' }} />
-          <Typography variant="body1" align="center" gutterBottom>
-            Address: {userDetails.address}
-          </Typography>
-          <Typography variant="body1" align="center" gutterBottom>
-            Email: {userDetails.email}
-          </Typography>
-          
-          <Button variant="contained" color="primary" onClick={()=>setIsEditDialogOpen(true)} sx={{ marginTop: '1rem' }}>
-            Update My Profile
-          </Button>
-          <Button variant="contained" color="secondary" onClick={handleViewAllOrders} sx={{ marginTop: '1rem' }}>
-            View My Orders
-          </Button>
-          <Dialog open={isEditDialogOpen} onClose={() => setIsEditDialogOpen(false)}>
-            <DialogTitle>Edit Profile</DialogTitle>
-            <DialogContent>
-              <TextField
-                label="Name"
-                fullWidth
-                value={editedName}
-                onChange={(e) => setEditedName(e.target.value)}
-                margin="normal"
-              />
-              <TextField
-                label="Address"
-                fullWidth
-                value={editedAddress}
-                onChange={(e) => setEditedAddress(e.target.value)}
-                margin="normal"
-              />
-              <TextField
-                label="Avatar URL"
-                fullWidth
-                value={editedAvatar}
-                onChange={(e) => setEditedAvatar(e.target.value)}
-                margin="normal"
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={() => setIsEditDialogOpen(false)} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={handleUpdateProfile} color="primary">
-                Update
-              </Button>
-            </DialogActions>
-          </Dialog>
-          <Dialog open={isOrdersDialogOpen} onClose={closeViewOrdersDialog}>
-            <DialogTitle>Your Orders</DialogTitle>
-            <DialogContent>
-              <List>
-                {userOrders.map((order) => (
-                  <ListItem key={order.id}>
-                    <ListItemText
-                      primary={`Order ID: ${order.id}`}
-                      secondary={
-                        <>
-                          <div>Order Status: {order.orderStatus}</div>
-                          <div>Order Date: {order.createAt}</div>
-                          <div>Order Products:</div>
-                          <ul>
-                            {orderProducts
-                              .filter(product => product.orderId === order.id)
-                              .map(product => (
-                                <li key={product.productId}>
-                                  {product.productId} - Quantity: {product.amount}
-                                </li>
-                              ))}
-                          </ul>
-                        </>
-                      }
-                    />
-                  </ListItem>
-                ))}
-              </List>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={closeViewOrdersDialog} color="primary">
-                Close
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </>
-      )}
-      {isLoggedIn && (
-        <IconButton
-          onClick={handleLogout}
-          color="primary"
-          sx={{ marginTop: '1rem' }}
-        >
-          <ExitToAppIcon />
-          <Typography variant="button">Log Out</Typography>
-        </IconButton>
-      )}
+        {role === 'User' && userDetails && (
+          <>
+            <Typography variant="h6" align="center" gutterBottom>
+              Your UserID: {userId}
+            </Typography>
+            <Typography variant="body1" align="center" gutterBottom>
+              Name: {name}
+            </Typography>
+            <img src={avatar} alt={`avatar`} width="100" style={{ cursor: 'pointer' }} />
+            <Typography variant="body1" align="center" gutterBottom>
+              Address: {userDetails.address}
+            </Typography>
+            <Typography variant="body1" align="center" gutterBottom>
+              Email: {userDetails.email}
+            </Typography>
+            
+            <Button variant="contained" color="primary" onClick={()=>setIsEditDialogOpen(true)} sx={{ marginTop: '1rem' }}>
+              Update My Profile
+            </Button>
+            <Button variant="contained" color="secondary" onClick={handleViewAllOrders} sx={{ marginTop: '1rem' }}>
+              View My Orders
+            </Button>
+            <Dialog open={isEditDialogOpen} onClose={() => setIsEditDialogOpen(false)}>
+              <DialogTitle>Edit Profile</DialogTitle>
+              <DialogContent>
+                <TextField
+                  label="Name"
+                  fullWidth
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                  margin="normal"
+                />
+                <TextField
+                  label="Address"
+                  fullWidth
+                  value={editedAddress}
+                  onChange={(e) => setEditedAddress(e.target.value)}
+                  margin="normal"
+                />
+                <TextField
+                  label="Avatar URL"
+                  fullWidth
+                  value={editedAvatar}
+                  onChange={(e) => setEditedAvatar(e.target.value)}
+                  margin="normal"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => setIsEditDialogOpen(false)} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleUpdateProfile} color="primary">
+                  Update
+                </Button>
+              </DialogActions>
+            </Dialog>
+            <Dialog open={isOrdersDialogOpen} onClose={closeViewOrdersDialog}>
+              <DialogTitle>Your Orders</DialogTitle>
+              <DialogContent>
+                <List>
+                  {userOrders.map((order) => (
+                    <ListItem key={order.id}>
+                      <ListItemText
+                        primary={`Order ID: ${order.id}`}
+                        secondary={
+                          <>
+                            <div>Order Status: {order.orderStatus}</div>
+                            <div>Order Date: {order.createAt}</div>
+                            <div>Order Products:</div>
+                            <ul>
+                              {orderProducts
+                                .filter(product => product.orderId === order.id)
+                                .map(product => (
+                                  <li key={product.productId}>
+                                    {product.productId} - Quantity: {product.amount}
+                                  </li>
+                                ))}
+                            </ul>
+                          </>
+                        }
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={closeViewOrdersDialog} color="primary">
+                  Close
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </>
+        )}
+        {isLoggedIn && (
+          <IconButton
+            onClick={handleLogout}
+            color="primary"
+            sx={{ marginTop: '1rem' }}
+          >
+            <ExitToAppIcon />
+            <Typography variant="button">Log Out</Typography>
+          </IconButton>
+        )}
+      </>
+      ) : ( <Typography>Loading...</Typography>)}
     </Container>
   );
 };
